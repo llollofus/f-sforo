e7f66b640ba3863c47a4c5e4b28415cc9df07360
llorenc <@>
2023-03-13 16:44:23 +0100

    nova branca

--- lll/src/llorenc.java
+++ lll/src/llorenc.java
@@ -2,4 +2,7 @@ public class llorenc {
     public static void main(String[] args) {
         System.out.println("clclacllcalcalcalou");
     }
+    public static void suma(){
+        System.out.println("sumandola");
+    }
 }
